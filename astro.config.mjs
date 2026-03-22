a2d87fc3ad544ae58792176fa6c1bdf253afb521
geraldb1996 <geraldb1996@gmail.com>
2026-03-22 13:27:07 -0600

    ⛩️  Preparando el Dojo para el despliegue final

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://geraldb1996.github.io',
+    base: '/dojohub',
     integrations: [tailwind()],
 });
